e50174849c0bb1ec88c5f374d425e2535a06a35d
Akash <pinnakaakash@gmail.com>
2024-06-27 18:49:34 -0400

    t

--- flink_app.tf
+++ flink_app.tf
@@ -1,44 +1,44 @@
 
-resource "aws_kinesisanalyticsv2_application" "flink_app" {
-  name                   = "nyc-taxi-flink-application"
-  runtime_environment    = "FLINK-1_8"
-  service_execution_role = aws_iam_role.example.arn
+# resource "aws_kinesisanalyticsv2_application" "flink_app" {
+#   name                   = "nyc-taxi-flink-application"
+#   runtime_environment    = "FLINK-1_8"
+#   service_execution_role = aws_iam_role.example.arn
 
-  application_configuration {
-    application_code_configuration {
-      code_content {
-        s3_content_location {
-          bucket_arn = aws_s3_bucket.data_bucket.arn
-          file_key   = aws_s3_object.example.key
-        }
-      }
+#   application_configuration {
+#     application_code_configuration {
+#       code_content {
+#         s3_content_location {
+#           bucket_arn = aws_s3_bucket.data_bucket.arn
+#           file_key   = aws_s3_object.example.key
+#         }
+#       }
     
-      code_content_type = "ZIPFILE"
-    }
+#       code_content_type = "ZIPFILE"
+#     }
 
 
 
-    flink_application_configuration {
-      checkpoint_configuration {
-        configuration_type = "DEFAULT"
-      }
+#     flink_application_configuration {
+#       checkpoint_configuration {
+#         configuration_type = "DEFAULT"
+#       }
 
-      monitoring_configuration {
-        configuration_type = "CUSTOM"
-        log_level          = "DEBUG"
-        metrics_level      = "TASK"
-      }
+#       monitoring_configuration {
+#         configuration_type = "CUSTOM"
+#         log_level          = "DEBUG"
+#         metrics_level      = "TASK"
+#       }
 
-      parallelism_configuration {
-        auto_scaling_enabled = true
-        configuration_type   = "CUSTOM"
-        parallelism          = 1
-        parallelism_per_kpu  = 1
-      }
-    }
-  }
+#       parallelism_configuration {
+#         auto_scaling_enabled = true
+#         configuration_type   = "CUSTOM"
+#         parallelism          = 1
+#         parallelism_per_kpu  = 1
+#       }
+#     }
+#   }
 
-  tags = {
-    env = "tf"
-  }
-}
+#   tags = {
+#     env = "tf"
+#   }
+# }
--- iam.tf
+++ iam.tf
@@ -1,34 +1,34 @@
-# Iam role for the Kinesis Analytics application
+# # Iam role for the Kinesis Analytics application
 
-resource "aws_iam_role" "nyc-kinesis-analytics-role" {
-  name = "nyc-kinesis-analytics-role"
+# resource "aws_iam_role" "nyc-kinesis-analytics-role" {
+#   name = "nyc-kinesis-analytics-role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "kinesisanalytics.amazonaws.com"
-        }
-      }
-    ]
-  })
+#   # Terraform's "jsonencode" function converts a
+#   # Terraform expression result to valid JSON syntax.
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "kinesisanalytics.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
 
-  tags = {
-    env = "tf"
-  }
-}
+#   tags = {
+#     env = "tf"
+#   }
+# }
 
-resource "aws_iam_role_policy" "nyc-kinesis-analytics-role-policy" {
-  name = "nyc-kinesis-analytics-role-policy"
-  role = aws_iam_role.nyc-kinesis-analytics-role.id
+# resource "aws_iam_role_policy" "nyc-kinesis-analytics-role-policy" {
+#   name = "nyc-kinesis-analytics-role-policy"
+#   role = aws_iam_role.nyc-kinesis-analytics-role.id
 
-  policy = jsonencode(
-    
-  )
-}
+#   policy = jsonencode(
+
+#   )
+# }
